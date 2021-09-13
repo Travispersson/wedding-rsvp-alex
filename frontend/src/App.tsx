00288b03f84bca8b5e18fed4d4808c486dc9c4ff
Travis Persson <travispersson93@gmail.com>
2021-09-13 20:50:45 +0200

    fixes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import GlobalStyle from "./styles/global"
-
+import HomePage from './components/HomePage';
 
 const App: React.FC = () => {
   return (
     <>
       <GlobalStyle />
+      <HomePage>
+
+      </HomePage>
     </>
   );
 }
--- frontend/src/components/HomePage/index.tsx
+++ frontend/src/components/HomePage/index.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-
-
-
-
-
+import StyledHomePage from "./styles"
 
 const HomePage: React.FC = () => {
 
-    return (<div>
-
-    </div>)
+    return (<StyledHomePage>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+    </StyledHomePage>)
 }
 
 export default HomePage
